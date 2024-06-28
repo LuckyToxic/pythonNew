4b77ac42ad049f87133846e1fe0d9dcfae7910e6
Evgeniy <151203550+LuckyToxic@users.noreply.github.com>
2024-06-28 15:10:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1488,3 +1488,61 @@ print(sing.val)   #10
 sing2 = Singleton(20)
 print(sing2.val)    # 10 
 print(sing == sing2)  # True 
+
+# Создайте реализацию паттерна Abstract Factory. Про-
+# тестируйте работу созданного класса.
+
+class WindowFactory:
+    @classmethod
+    def create_window(cls,name):
+        return cls.Window(name)
+    @classmethod
+    def create_button(cls,name):
+        return cls.Button(name)
+    
+class MacOsFactory(WindowFactory):
+    class Window:
+        def __init__(self,name):
+            self.name = name
+            self.button = []
+            self.style = 'Mac Os window style'
+
+        def add_button(self,btn):
+            self.button.append(btn.name)
+
+        def show(self):
+            print(f'{self.name} - {self.style} and {self.button}')
+
+    class Button:
+        def __init__(self,name):
+            self.name = name
+            self.style = 'Mac Os button style'
+
+class LinuxFactory(WindowFactory):
+    class Window:
+        def __init__(self,name):
+            self.name = name
+            self.button = []
+            self.style = 'Ubuntu window style'
+
+        def add_button(self,btn):
+            self.button.append(btn.name)
+
+        def show(self):
+            print(f'{self.name} - {self.style} and {self.button}')
+    class Button:
+        def __init__(self,name):
+            self.name = name 
+            self.style = 'Ubuntu button style'
+
+def create_dialog(factory):
+    wind = factory.create_window('Form1')
+    button = factory.create_button('Button1')
+    wind.add_button(button)
+    return wind
+
+create_dialog(LinuxFactory) 
+
+w = WindowFactory.create_window(LinuxFactory)
+
+w.show()
